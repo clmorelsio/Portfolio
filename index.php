4026f883da377b9759d8a1e269bb7d8493371435
ashoka <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>
2024-12-08 19:34:35 +0100

    Basic header and footer set up

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+	<link rel="stylesheet" href="./portfolio.css">
+</head>
 
-echo "<h1>Welcome to my site</h1>";
+<body>
+    <header>
+	<ul>
+		<li><a href="#accueil">ACCUEIL</a></li>
+		<li><a href="#competences">COMPETENCES</a></li>
+		<li><a href="#realisations">REALISATIONS</a></li>
+		<li><a href="#formation">FORMATION</a></li>
+		<li><a href="#contact">CONTACT</a></li>
+	</ul>
+    </header>
 
-include_once("./php/accueil.php");
-include_once("./php/competences.php");
-include_once("./php/realisations.php");
-include_once("./php/formation.php");
-include_once("./php/contact.php");
+    <main>
+        <?php
 
-?>
+            echo "<h1>Welcome to my site</h1>";
+
+            include_once("./php/accueil.php");
+            include_once("./php/competences.php");
+            include_once("./php/realisations.php");
+            include_once("./php/formation.php");
+            include_once("./php/contact.php");
+
+        ?>
+    </main>
+
+</body>
+</html>
