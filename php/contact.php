b6f062ebadf082254b3ab05762027e1f34d222ed
ashoka <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>
2024-12-08 20:47:22 +0100

    Basic formulaire without captcha

--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,30 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+</head>
 
-require_once("yaml/yaml.php");
-$data=yaml_parse_file("../YAML/contact.yaml");
+<body>
+    <form method="POST" action="/actions/submit.php"> 
+        //action here is just a placeholder, put the real one in later
 
-echo "<h1>".$data["titre"]."</h1>\n";
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" placeholder="Votre nom">
 
-?>
+        <label for="adresseMail">Adresse mail :</label>
+        <input type="email" id="adresseMail" placeholder="Votre adresse mail">
+
+        <label for="objet">Objet :</label>
+        <input type="text" id="objet" placeholder="L'objet de votre message">
+
+        <label for="message">Message :</label>
+        <input type="text" id="message" placeholder="Votre message">
+
+        // captcha here
+
+        <input type="submit" value="Envoyer">
+
+    </form>
+</body>
+
